c923d1e2d8eeffcf1cd929b9e9c16105b1c672d7
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2023-10-23 22:00:56 -0400

    Started exercise 3.

---
+++ ch12/Fraction.h
@@ -0,0 +1,10 @@
+struct Fraction {
+    int numerator;
+    int denomintator;
+
+    // These are all modifier functions:
+    void add(Fraction frac);
+    void subtract(Fraction frac);
+    void multiply(Fraction frac);
+    void divide(Fraction frac);
+};
